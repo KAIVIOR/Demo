ad8a41bd4b6b02294e7d97be5da45e43995efc78
ericdddddd <boy0991091@gmail.com>
2020-09-28 16:18:21 +0800

    Initial Commit

---
+++ KAIIII/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KAIIII
+//
+//  Created by User13 on 2020/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KAIIII/KAIIIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KAIIIIApp.swift
+//  KAIIII
+//
+//  Created by User13 on 2020/9/28.
+//
+
+import SwiftUI
+
+@main
+struct KAIIIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
